7c8e31edac8172a0e5519c9a0f97889f6373a23c
Vedant Heda <vedant.heda@outlook.com>
2024-03-21 03:09:33 -0700

    Initial commit

---
+++ flutter-test-main/lib/data/models/counter.dart
@@ -0,0 +1,13 @@
+/// A simple data model representing a counter.
+class Counter {
+  /// The current count value of the counter.
+  final int count;
+
+  /// A boolean property indicating whether the counter value is positive.
+  bool get isPositive => count > 0;
+
+  bool get isNegative => count < 0; 
+
+  /// Constructor for creating a Counter instance with an initial count.
+  Counter(this.count);
+}
